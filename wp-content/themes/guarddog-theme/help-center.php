9be5affb66506a32522775e41e8e44d5bdce563c
DanielViglione <dviglione02@gmail.com>
2012-07-10 18:36:54 -0400

    added some new pages

---
+++ wp-content/themes/guarddog-theme/help-center.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Template Name: Help Center
+*/
+?>
+<?php get_header(); ?> 
+	<?php while (have_posts()) : the_post(); ?>    
+	  <?php the_content(); ?>
+	<?php endwhile; ?>
+<?php get_footer(); ?>
